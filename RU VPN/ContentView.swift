8db232b4caec4234f51abcf129d00ed8f0eb0882
Ed <Siniakov>
2022-12-06 21:23:28 +0300

    Initial Commit

---
+++ RU VPN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RU VPN
+//
+//  Created by Eduard Kanevskii on 06.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RU VPN/RU_VPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  RU_VPNApp.swift
+//  RU VPN
+//
+//  Created by Eduard Kanevskii on 06.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RU_VPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
